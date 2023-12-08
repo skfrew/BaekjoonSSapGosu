bf13158e3bf0d9ccb4053eacdc39448e962cbcf0
KANGJIHEON <133093827+ziphyun@users.noreply.github.com>
2023-12-08 23:32:28 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, result=0;
+  for (int i = 0; i < 5; i++) {
+    cin >> a;
+    result += a*a;
+  }
+  cout << result % 10;
+  return 0;
+}
